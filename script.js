fb7ca6e86b15ab8686d098b0aab7cdfd7d8f4d14
Tococo852 <tococo852@gmail.com>
2025-01-30 19:04:58 -0300

    added the stektch drawing cotiner, and the ability to draw on it with transparency increases

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
 
-function addBox(){
+
+
+function addColumns(row){
     const sketchFrame= document.createElement('div',);
-    sketchFrame.setAttribute('class', 'sketchFrame box');
-    const sketchBox= document.querySelector('#SketchContent');
-    sketchBox.appendChild(sketchFrame);
-    console.log('?')
+    sketchFrame.setAttribute('class', 'sketchColumn boxShadow');
+    
+    row.appendChild(sketchFrame);
+
 }
 
+function addRows(columns){
+    const sketchFrame= document.createElement('div',);
+    sketchFrame.setAttribute('class', 'sketchRow');
+    const sketchBox= document.querySelector('#SketchContent');
+    for(let i=0; i<columns; i++){
+        addColumns(sketchFrame)
+    }
+    
+    sketchBox.appendChild(sketchFrame);
 
 
-function makeMatrix(columns, rows){
+
+}
+
+function makeMatrix( rows, columns){
     for(let i=0; i<rows; i++){
-        for(let c=0; c<columns; c++){
-            addBox();
-        }
+        addRows(columns);
     }
 }
+const drawbox=document.querySelector('#SketchContent');
 
-makeMatrix(16,16)
 
+
+makeMatrix(100,100)
+
+let color='blue'
+
+drawbox.addEventListener('mouseover', function(e){
+    switch(e.target.className){
+        case 'sketchColumn boxShadow':
+            e.target.style.background=color;
+
+            let currOpacity= parseFloat(e.target.style.opacity) || 0;
+            let newOpacity= Math.min(currOpacity+0.1, 1)
+            e.target.style.opacity=newOpacity;
+            
+
+
+
+
+    }
+})
